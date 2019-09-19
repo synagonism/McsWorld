856d7b503663da0e58c4da9f022de2c46b1a2fc5
Kaseluris.Nikos.1959 <synagonism@gmail.com>
2019-09-19 20:55:09 +0300

    2019-09-19.space-time-Sympan

--- js-mcsAdd3.js
+++ js-mcsAdd3.js
@@ -36,13 +36,13 @@ var
   sDir = 'dirCor',
   sDIR = 'DirCor',
   //3. Name of the-title: Ethereum--blockchain-net
-  sN = 'abstract-quantity',
+  sN = 'Sympan',
   //4. SHORT-name: DnChain, DnEth, Dchain-net, lagKmo,
-  sNS = 'quantity',
+  sNS = 'Sympan',
   //5. FILE-name: shows relation of another file: DnLbr, DtcbnetEth
-  sNFil = 'Qnt',
+  sNFil = 'Smpn',
   //6. Name for IDs eg Dtc, unique in this file.
-  sNId = 'Qnt';
+  sNId = 'Spn';
 
 s =
   '<!DOCTYPE html>\n' +
