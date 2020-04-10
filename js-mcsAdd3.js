06d103004310b5dbeb14504f1c066ad062a3b387
Kaseluris.Nikos.1959 <synagonism@gmail.com>
2020-04-10 11:52:19 +0300

    2020-04-10.system

--- js-mcsAdd3.js
+++ js-mcsAdd3.js
@@ -40,16 +40,16 @@ var
   bCounter = true,
   bDisqus = false, //on meta-info on dir-pages
   //2. Directory of the-concept (dirCor or else)
-  sDir = 'dirOgm',
-  sDIR = 'DirOgm',
+  sDir = 'dirCor',
+  sDIR = 'DirCor',
   //3. Name of the-title: Ethereum--blockchain-net
-  sN = 'organism-system-of-molecules',
+  sN = 'whole.system',
   //4. SHORT-name: ogn, DnChain, DnEth, Dchain-net, lagKmo,
-  sNS = 'sysMoleculesOgm',
+  sNS = 'system',
   //5. FILE-name: shows relation of another file: DtcbnetEth, DnLbr,
-  sNFil = 'SysMlclOgm',
+  sNFil = 'Sys',
   //6. Name for IDs eg Dtc, unique in this file.
-  sNId = 'SysMlclOgm';
+  sNId = 'Sys';
 
 s =
   '<!DOCTYPE html>\n' +
