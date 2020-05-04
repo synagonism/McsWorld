6b2dbf6335175de42b576b06568e29175d1285aa
Kaseluris.Nikos.1959 <synagonism@gmail.com>
2020-05-04 13:04:25 +0300

    2020-05-04.Cpgm

--- js-mcsAdd3.js
+++ js-mcsAdd3.js
@@ -40,16 +40,16 @@ var
   bCounter = true,
   bDisqus = false, //on meta-info on dir-pages
   //2. Directory of the-concept (dirCor or else)
-  sDir = 'dirOgm',
-  sDIR = 'DirOgm',
+  sDir = 'dirTchCpgm',
+  sDIR = 'dirTchCpgm',
   //3. Name of the-title: Ethereum--blockchain-net
-  sN = 'organism.animal',
+  sN = 'computer-program',
   //4. SHORT-name: ogn, DnChain, DnEth, Dchain-net, lagKmo,
-  sNS = 'ogmAnimal',
+  sNS = 'Cpgm',
   //5. FILE-name: shows relation of another file: DtcbnetEth, DnLbr,
-  sNFil = 'OgmAnml',
+  sNFil = 'Cpgm',
   //6. Name for IDs eg Dtc, unique in this file.
-  sNId = 'OgmAnml';
+  sNId = 'Cpgm';
 
 s =
   '<!DOCTYPE html>\n' +
