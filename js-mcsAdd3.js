4ae01370f89370338240e59b435952237cf2cc01
Kaseluris.Nikos.1959 <synagonism@gmail.com>
2020-11-02 20:05:35 +0200

    2020-11-02.Earth

--- js-mcsAdd3.js
+++ js-mcsAdd3.js
@@ -41,16 +41,16 @@ var
   bCounter = true,
   bDisqus = false, //on meta-info on dir-pages
   //2. Directory of the-concept (dirCor or else)
-  sDir = 'dirStn',
-  sDIR = 'dirStn',
+  sDir = 'dirNtr',
+  sDIR = 'dirNtr',
   //3. Name of the-title: Ethereum--blockchain-net
-  sN = 'socialitation.project',
+  sN = 'Nature\'Earth',
   //4. SHORT-name: sysNet, ogn, DnChain, DnEth, Dchain-net, lagKmo,
-  sNS = 'project',
+  sNS = 'Earth',
   //5. FILE-name: shows relation of another file: SysNet, DtcbnetEth, DnLbr,
-  sNFil = 'Project',
+  sNFil = 'Erth',
   //6. Name for IDs eg Dtc, unique in this file: Net,
-  sNId = 'Pjt';
+  sNId = 'Erth';
 
 s =
   '<!DOCTYPE html>\n' +
