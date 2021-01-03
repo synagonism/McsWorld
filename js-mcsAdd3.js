224caa9117a681dc4cbde99082d6747c8841576a
Kaseluris.Nikos.1959 <synagonism@gmail.com>
2021-01-03 10:02:00 +0200

    2021-01-03.web-standard

--- js-mcsAdd3.js
+++ js-mcsAdd3.js
@@ -43,16 +43,16 @@ var
   bCounter = true,
   bDisqus = false, //on meta-info on dir-pages
   //2. Directory of the-concept (dirCor or else)
-  sDir = 'dirTchInf',
-  sDIR = 'DirTchInf',
+  sDir = 'dirStn',
+  sDIR = 'DirStn',
   //3. Name of the-title: Ethereum--blockchain-net
-  sN = 'knowledge-representation-language',
+  sN = 'standards-organization',
   //4. SHORT-name: sysNet, ogn, DnChain, DnEth, Dchain-net, lagKmo,
-  sNS = 'lagKr',
+  sNS = 'oznStd',
   //5. FILE-name: shows relation of another file: SysNet, DtcbnetEth, DnLbr,
-  sNFil = 'Lkr',
+  sNFil = 'OznStd',
   //6. Name for IDs eg Dtc, unique in this file: Net,
-  sNId = 'Lkr';
+  sNId = 'OznStd';
 
 s =
   '<!DOCTYPE html>\n' +
