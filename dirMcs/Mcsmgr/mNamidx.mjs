e75bea7a4dae038cebe9d86e85fc0138a4ef3695
Kaseluris.Nikos.1959 <synagonism@gmail.com>
2023-06-11 10:46:47 +0300

    2023-06-10.doing

--- dirMcs/Mcsmgr/mNamidx.mjs
+++ dirMcs/Mcsmgr/mNamidx.mjs
@@ -130,7 +130,7 @@ function fNamidx(fileIn, fSftpIn) {
     // array with names of dirCor/McsCor000010.last.html to remove|add its names
     aLag,
     // array of languages ['lagALLL'] or ['lagElln','lagEngl',...]
-    aLagALL = ['lagEngl'],
+    aLagALL = ['lagEngl','lagZhon'],
     //aLagALL = ['lagEngl','lagSngo','lagZhon','lagEspo','lagElln','lagElla','lagTurk'],
     // 'lagAlb','lagBel','lagBos','lagBul','lagCes','lagDan','lagDeu','lagEst',
     // 'lagFin','lagFra','lagHrv','lagHun','lagIta','lagLav','lagLit','lagMlt',
